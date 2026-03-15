a1ad300ff9bcf631fe3c76d4dc0efef6647fd739
Hasan Demirtaş <utsukushihito@outlook.com>
2026-03-15 18:09:24 +0300

    Update builder.dart

--- packages/xml_object_mapping_generator/lib/src/builder.dart
+++ packages/xml_object_mapping_generator/lib/src/builder.dart
@@ -3,7 +3,5 @@ import "package:source_gen/source_gen.dart";
 import "package:xml_object_mapping_generator/xml_object_mapping_generator.dart";
 
 /// Builder for generating XML mapper classes.
-Builder xmlMapperBuilder(BuilderOptions options) => LibraryBuilder(
-  XmlMapperGenerator(),
-  generatedExtension: ".xml_mapper.g.dart",
-);
+Builder xmlMapperBuilder(BuilderOptions options) =>
+    SharedPartBuilder([XmlMapperGenerator()], "xml_mapper");
